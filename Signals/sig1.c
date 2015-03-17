018855f39704afa70d9d5f3f1d8c35592ab56313
TimPhoeniX <tim-phoenix@royal.net>
2015-03-17 16:40:33 +0100

    17.03.15
    First Commit

---
+++ Signals/sig1.c
@@ -0,0 +1,28 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<signal.h>
+#include<stdlib.h>
+
+void sighandler(int sig)
+{
+	puts("Otrzymano SIGINT");
+	exit(EXIT_SUCCESS);
+}
+
+int main()
+{
+	if (signal(SIGQUIT,SIG_IGN) == SIG_ERR)
+	{
+		perror("Problem z SIGQUIT");
+		exit(EXIT_FAILURE);
+	}
+	if (signal(SIGINT,sighandler) == SIG_ERR)
+	{
+		perror("Problem z SIGINT");
+		exit(EXIT_FAILURE);
+	}
+	
+	pause();
+	
+	exit(0);
+}
