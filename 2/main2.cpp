cc28448008d4f25dc4cddd39e45996d01b195f9f
TimPhoeniX <tim-phoenix@royal.net>
2015-10-09 11:26:12 +0200

    Restarting repository

--- 2/main2.cpp
+++ 2/main2.cpp
@@ -1,9 +1,10 @@
-insertion-sort(int* A)
+insertion_sort(int* A, int size)
 {
+//	for(int j=1; j< length(A);++j)
 	for(int j=1; j< length(A);++j)
 	{
 		int key=A[j];
-		i=j-1;
+		int i=j-1;
 		while i>=0 && A[i]>key)
 		{
 			A[i+1]=A[i];
--- README.md
+++
@@ -1,3 +0,0 @@
-# AiSD
-AiSD UJ 2015/2016
-Do not use at UJ.
