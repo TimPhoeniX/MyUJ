61196de9decf25427559e45c9ce24a20c2b1098a
TimPhoeniX <tim-phoenix@royal.net>
2015-11-24 00:23:38 +0100

    RPN nearly finished

--- 8/test.cpp
+++ 8/test.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-
-
+#include <cmath>
 
 int main()
 {
@@ -15,15 +14,74 @@ int main()
 	{
 		Stream.str(Exp);
 		Stream.clear();
+		int val = 0;
+		int b = 0;
+		int a = 0;
+		std::string sb = "";
+		std::string sa = "";
 		while(!Stream.eof())
 		{
 			auto c = Stream.get();
-			std::cout << c << std::endl;
 			switch(c)
 			{
-				case ' ': break;
+				case ' ':
+				case -1: 
+					break;
+				case '+':
+					b = Int.Pop();
+					a = Int.Pop();
+					sb = Expressions.Pop();
+					sa = Expressions.Pop();
+					Int.Push(a+b);
+					Expressions.Push('('+sa+'+'+sb+')');
+					break;
+				case '-':
+					b = Int.Pop();
+					a = Int.Pop();
+					sb = Expressions.Pop();
+					sa = Expressions.Pop();
+					Int.Push(a-b);
+					Expressions.Push('('+sa+'-'+sb+')');
+					break;
+				case '*':
+					b = Int.Pop();
+					a = Int.Pop();
+					sb = Expressions.Pop();
+					sa = Expressions.Pop();
+					Int.Push(a*b);
+					Expressions.Push(sa+'*'+sb);
+					break;
+				case '/':
+					b = Int.Pop();
+					a = Int.Pop();
+					sb = Expressions.Pop();
+					sa = Expressions.Pop();
+					Int.Push(a/b);
+					Expressions.Push(sa+'/'+sb);
+					break;
+				case '^':
+					b = Int.Pop();
+					a = Int.Pop();
+					sb = Expressions.Pop();
+					sa = Expressions.Pop();
+					Int.Push(std::pow(a,b));
+					Expressions.Push('('+sa+")^("+sb+')');
+					break;
+				case '~':
+					a = Int.Pop();
+					sa = Expressions.Pop();
+					Int.Push(-a);
+					Expressions.Push("(-"+sa+')');
+					break;
+				default:
+					Stream.unget();
+					Stream >> val;
+					Int.Push(val);
+					Expressions.Push(std::to_string(val));
+					break;
 			}
 		}
+		std::cout << Int.Pop() << '\n' << Expressions.Pop() << std::endl;
 	}
 	
 	return 0;
