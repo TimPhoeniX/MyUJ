579770983b27c91fd91ab1eadba0a7f5f63451c1
TimPhoeniX <tim-phoenix@royal.net>
2016-03-22 17:43:24 +0100

    GOTO in AiSD 3

--- AiSD/3/suma.cpp
+++ AiSD/3/suma.cpp
@@ -19,6 +19,15 @@ double TailSum(int n, double s)
 		return TailSum(n-1, s+1.);
 }
 
+double EliminatedTailSum(int n, double s)
+{
+	lab:
+	if(n==1)
+		return s;
+	else
+	{ n = n - 1; s = s + 1; goto lab; }
+}
+
 int main(void)
 {
 	constexpr int NumberOfExecutions = 2000000;
@@ -48,6 +57,13 @@ int main(void)
 	s = SumOfOnes(BiggerNumber);
 	t4 = std::clock() - t4;
 	std::cout << "Suma jedynek: " << std::setiosflags(std::ios::fixed) << std::setprecision(0) << std::setw(10) << s << " clock ticks: " << t4 << '\n';
+	
+	volatile std::clock_t t5 = std::clock();
+	// Komilacja przez G++ z -O2 i -O3 doprowadzi do przepełnienia stosu w tym wywolaniu
+	s = EliminatedTailSum(BiggerNumber,1);
+	t5 = std::clock() - t5;
+	std::cout << "Suma GOTO   : " << std::setiosflags(std::ios::fixed) << std::setprecision(0) << std::setw(10) << s << " clock ticks: " << t5 << '\n';
+	
 	
 	return 0;
 }
